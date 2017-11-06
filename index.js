27594a95895f967934fe92f2d64123b493e062b8
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-11-05 21:18:29 -0500

    finish output messages synced with color and level

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var lodash_var=  require('lodash');
+var _=  require('lodash');
 var chalk_var=  require('chalk');
-console.log(chalk_var.blue("what up"));
+var logs=require('./data/logs');
+var logger=require('./lib/logger');
 
-var phonetic = ["alpha","bravo","charlie","delta","echo","foxtrot","golf","hotel","india","julliett","kilo","lima","mike","november","oscar","papa","quebec","romeo","sierra","tango","uniform","victor","whiskey","xray","yankee","zulu"];
-lodash_var.each(phonetic, function(letter){
-  console.log(letter);
-})
+_.each(logs, log =>{
+  logger(log.message, log.level);
+});
